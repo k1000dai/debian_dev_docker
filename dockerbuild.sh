adbc342e7fe60c7e118f0c9edb8bf2405520d575
k1000dai <chiyodakku1000@gmail.com>
2025-05-10 10:53:57 +0200

    chage name

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 DOCKERFILE=cpu/Dockerfile
-TAG=sid-kohei-dev:cpu
+TAG=debian-sid-dev:cpu
 
 if command -v nvidia-smi >/dev/null 2>&1; then
     echo "NVIDIA GPU detected. Using GPU version tag."
     DOCKERFILE=gpu/Dockerfile
-    TAG=sid-kohei-dev:gpu
+    TAG=debian-sid-dev:gpu
 fi
 
 docker build -t ${TAG} -f ${DOCKERFILE} . 
--- rundocker.sh
+++ rundocker.sh
@@ -2,12 +2,12 @@
 if command -v nvidia-smi >/dev/null 2>&1; then
     echo "NVIDIA GPU detected. Using GPU version tag."
     docker run --platform=linux/amd64  \
-        --name sid-kohei-container --privileged --shm-size=4g -it \
+        --name debian-sid-container --privileged --shm-size=4g -it \
         --gpus all --env NVIDIA_DISABLE_REQUIRE=1 \
-        sid-kohei-dev:latest bash
+        debian-sid-dev:latest bash
 else
     echo "No NVIDIA GPU detected. Using CPU version tag."
     docker run --platform=linux/amd64 \
-        --name sid-kohei-container --privileged \
-        --shm-size=4g -it sid-kohei-dev:cpu bash
+        --name debian-sid-container --privileged \
+        --shm-size=4g -it debian-sid-dev:cpu bash
 fi
