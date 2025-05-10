4c27e9a009b8f2b1f81ddda4a1a82f8648e6a3c9
k1000dai <chiyodakku1000@gmail.com>
2025-05-10 11:02:07 +0200

    fix latest to gpu

--- rundocker.sh
+++ rundocker.sh
@@ -4,7 +4,7 @@ if command -v nvidia-smi >/dev/null 2>&1; then
     docker run --platform=linux/amd64  \
         --name debian-sid-container --privileged --shm-size=4g -it \
         --gpus all --env NVIDIA_DISABLE_REQUIRE=1 \
-        debian-sid-dev:latest bash
+        debian-sid-dev:gpu bash
 else
     echo "No NVIDIA GPU detected. Using CPU version tag."
     docker run --platform=linux/amd64 \
