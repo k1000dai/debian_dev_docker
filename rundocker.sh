81e92848869910a17f413023fdeac3e4c82c6241
k1000dai <chiyodakku1000@gmail.com>
2025-05-25 20:59:24 +0200

    add --network=host

--- rundocker.sh
+++ rundocker.sh
@@ -2,6 +2,7 @@
 if command -v nvidia-smi >/dev/null 2>&1; then
     echo "NVIDIA GPU detected. Using GPU version tag."
     docker run --platform=linux/amd64  \
+        --network=host \
         --name debian-sid-container --privileged --shm-size=4g -it \
         --gpus all --env NVIDIA_DISABLE_REQUIRE=1 \
         debian-sid-dev:gpu bash
