5d2e1aca191079c2fa0044497bd1c1e42b8988b1
k1000dai <chiyodakku1000@gmail.com>
2025-06-06 23:33:03 +0900

    add cme

--- run_from_ghcr.sh
+++ run_from_ghcr.sh
@@ -2,6 +2,8 @@ docker run -dit --privileged --platform=linux/amd64 \
     --name debian-sid-container --shm-size=4g \
     ghcr.io/k1000dai/debian_dev_docker:main
 
-docker cp ~/.ssh debian-sid-container:/root/.ssh
+docker cp ~/.ssh debian-sid-container:/home/devuser/
+
+docker exec -it debian-sid-container sudo chown -R devuser:devuser /home/devuser/.ssh
 
 docker exec -it debian-sid-container bash
