17252e6e0e06facebf27d7b3934d6b9280ea0555
k1000dai <chiyodakku1000@gmail.com>
2025-08-21 14:37:29 +0900

    add shared mem

--- rundocker.sh
+++ rundocker.sh
@@ -3,12 +3,12 @@ if command -v nvidia-smi >/dev/null 2>&1; then
     echo "NVIDIA GPU detected. Using GPU version tag."
     docker run --platform=linux/amd64  \
         --network=host \
-        --name debian-sid-container --privileged --shm-size=4g -it \
+        --name debian-sid-container --privileged --shm-size=50g -it \
         --gpus all --env NVIDIA_DISABLE_REQUIRE=1 \
         debian-sid-dev:gpu bash
 else
     echo "No NVIDIA GPU detected. Using CPU version tag."
     docker run --platform=linux/amd64 \
         --name debian-sid-container --privileged \
-        --shm-size=4g -it debian-sid-dev:cpu bash
+        --shm-size=50g -it debian-sid-dev:cpu bash
 fi
