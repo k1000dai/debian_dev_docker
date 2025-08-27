982c0b31bd98259b1bc1803e5fa840bc3acf22ef
k1000dai <chiyodakku1000@gmail.com>
2025-08-27 09:05:51 +0900

    Rundocker

--- rundocker.sh
+++ rundocker.sh
@@ -3,12 +3,17 @@ if command -v nvidia-smi >/dev/null 2>&1; then
     echo "NVIDIA GPU detected. Using GPU version tag."
     docker run --platform=linux/amd64  \
         --network=host \
-        --name debian-sid-container --privileged --shm-size=50g -it \
+        --name debian-sid-container --privileged --cap-add=SYS_ADMIN \
+        --security-opt seccomp=unconfined \
+        --security-opt apparmor=unconfined \
+        --shm-size=50g -it \
         --gpus all --env NVIDIA_DISABLE_REQUIRE=1 \
         debian-sid-dev:gpu bash
 else
     echo "No NVIDIA GPU detected. Using CPU version tag."
     docker run --platform=linux/amd64 \
-        --name debian-sid-container --privileged \
+        --name debian-sid-container --privileged --cap-add=SYS_ADMIN \
+        --security-opt seccomp=unconfined \
+        --security-opt apparmor=unconfined \
         --shm-size=50g -it debian-sid-dev:cpu bash
 fi
